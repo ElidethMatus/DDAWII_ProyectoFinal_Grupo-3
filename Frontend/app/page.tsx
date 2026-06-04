a713a16fb9e5f0f1023c1374d182cf24a9561eef
Jason Madrid <jasonhnn6@gmail.com>
2026-06-03 19:32:28 -0600

    Agregando js bootstrap

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,15 +1,5 @@
-import Image from "next/image";
+import Navbar from "./shared/components/navbar/ui/Navbar/Navbar";
 
 export default function Home() {
-  return (
-    <div className="container mt-5">
-      <div className="container text-center">
-        <div className="row">
-          <div className="col bg-secondary">Column</div>
-          <div className="col bg-primary">Column</div>
-          <div className="col bg-success">Column</div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Navbar />;
 }
